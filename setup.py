2254542291037acc2afbacc5810ae8b7e6cda88a
INADA Naoki <inada-n@klab.com>
2013-02-25 10:22:20 +0900

    0.6

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name="wsaccel",
-      version='0.5',
+      version='0.6',
       description="Accelerator for ws4py and AutobahnPython",
       maintainer="INADA Naoki",
       maintainer_email="songofacandy@gmail.com",
